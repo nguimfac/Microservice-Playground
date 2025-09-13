de10356c1f12147321472edbff5fcfad7f4b17f1
nguimfackjunior2 <nguimfackjunior2@gmail.com>
2025-09-13 23:53:52 +0400

    breaking_change:try fixing db connection for invtr and order service

--- init.sql
+++ init.sql
@@ -1,2 +1,13 @@
-CREATE DATABASE IF NOT EXISTS orderdb;
-CREATE DATABASE IF NOT EXISTS inventorydb;
+-- Création base de données pour Order Service
+CREATE DATABASE IF NOT EXISTS order_db;
+CREATE USER IF NOT EXISTS 'order_user'@'%' IDENTIFIED BY 'order_pass';
+GRANT ALL PRIVILEGES ON order_db.* TO 'order_user'@'%';
+
+-- Création base de données pour Inventory Service
+CREATE DATABASE IF NOT EXISTS inventory_db;
+CREATE USER IF NOT EXISTS 'inventory_user'@'%' IDENTIFIED BY 'inventory_pass';
+GRANT ALL PRIVILEGES ON inventory_db.* TO 'inventory_user'@'%';
+
+-- Appliquer les changements
+FLUSH PRIVILEGES;
+
