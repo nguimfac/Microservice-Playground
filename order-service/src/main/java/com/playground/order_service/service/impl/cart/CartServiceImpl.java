249fbcd6442e62c6849a8e571fe825c4722624df
nguimfackjunior2 <nguimfackjunior2@gmail.com>
2025-09-27 23:28:51 +0400

    change:remove unuse transactionnal annotation on method addProductToCart

--- order-service/src/main/java/com/playground/order_service/service/impl/cart/CartServiceImpl.java
+++ order-service/src/main/java/com/playground/order_service/service/impl/cart/CartServiceImpl.java
@@ -48,7 +48,6 @@ public class CartServiceImpl  implements CartService {
         strategies.put(CartStrategyEnum.BUNDLE,  new BundleCartStrategyImpl());
     }
 
-    @Transactional
     public CartResponse addProductToCart(long cartId,AddProductRequest addProductRequest) {
         log.info("Try to add product to cart id {}", cartId);
         CartStrategy strategy = strategies.getOrDefault(addProductRequest.strategy(), new DefaultCartStrategyImpl(cartRepository));
