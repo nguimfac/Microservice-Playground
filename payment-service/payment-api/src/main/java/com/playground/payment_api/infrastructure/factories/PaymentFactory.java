3fc679f3fccc055b452b78b1a694e5a7a03ca479
arnold1902 <afotsofone@gmail.com>
2025-10-01 23:24:37 +0400

    feat: Refactor PaymentFactory to interface and implement PaymentFactoryImpl

--- payment-service/payment-api/src/main/java/com/playground/payment_api/infrastructure/factories/PaymentFactory.java
+++ payment-service/payment-api/src/main/java/com/playground/payment_api/infrastructure/factories/PaymentFactory.java
@@ -1,25 +1,7 @@
 package com.playground.payment_api.infrastructure.factories;
 
-import java.util.Map;
-
-import org.springframework.stereotype.Component;
-
-import com.playground.payment.core.exceptions.UnknownProviderException;
 import com.playground.payment_api.domain.ports.inbound.PaymentService;
 
-import lombok.RequiredArgsConstructor;
-
-@Component
-@RequiredArgsConstructor
-public class PaymentFactory {
-
-    private final Map<String, PaymentService> services;
-
-    public PaymentService getStrategy(String provider) {
-        PaymentService svc = services.get(provider);
-        if (svc == null) {
-            throw new UnknownProviderException(provider);
-        }
-        return svc;
-    }
+public interface PaymentFactory {
+    PaymentService getStrategy(String provider);
 }
--- payment-service/payment-web/src/main/java/com/playground/payment_web/PaymentWebApplication.java
+++ payment-service/payment-web/src/main/java/com/playground/payment_web/PaymentWebApplication.java
@@ -3,11 +3,13 @@ package com.playground.payment_web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "com.playground.payment_web",
+    "com.playground.payment_api", 
+    "com.playground.payment_db"
+})
 public class PaymentWebApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentWebApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentWebApplication.class, args);
+    }
 }
