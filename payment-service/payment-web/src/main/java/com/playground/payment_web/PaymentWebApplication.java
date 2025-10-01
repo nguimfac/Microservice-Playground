7a2de963107ee6cda459e386cdacaafe58d8b4a3
arnold1902 <afotsofone@gmail.com>
2025-10-02 01:36:13 +0400

    feat: Add H2 and PostgreSQL dependencies for database support; enable Eureka client in PaymentWebApplication

--- payment-service/payment-web/src/main/java/com/playground/payment_web/PaymentWebApplication.java
+++ payment-service/payment-web/src/main/java/com/playground/payment_web/PaymentWebApplication.java
@@ -2,7 +2,9 @@ package com.playground.payment_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {
     "com.playground.payment_web",
     "com.playground.payment_api", 
